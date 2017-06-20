c5cf0786a992bdcba6e1f229516b65756638e057
Majeed Kazemitabaar <majeed@umd.edu>
2017-06-20 16:07:05 -0700

    dataStreamer is now correct

--- target/DataStreamer/main.ts
+++ target/DataStreamer/main.ts
@@ -1,3 +1,23 @@
+// Timing
+const sampleRate = 40; // 25fps
+let prevTime = 0;
+
+//The main loop: 
 loops.forever(() => {
-	
+    while (true) {
+        let time = control.millis();
+
+        // If enough time has elapsed or the timer rolls over, do something
+        if ((time - prevTime) >= sampleRate || time < prevTime) {
+            serial.writeLine("ACC " + input.acceleration(Dimension.X) + " "
+                + input.acceleration(Dimension.Y) + " "
+                + input.acceleration(Dimension.Z) +
+                " ROT " + input.rotation(Rotation.Pitch) + " "
+                + input.rotation(Rotation.Roll));
+
+            prevTime = time;
+        }
+
+        loops.pause(1);
+    }
 })
